0546e5f31a72d578e423adbce395d3e1496670fc
Faaiz Muzzammil <faaizmuzzammil@gmail.com>
2024-12-29 19:12:29 +0500

    Made Basic Player UI

--- music_player/lib/views/player.dart
+++ music_player/lib/views/player.dart
@@ -9,7 +9,9 @@ class Player extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
-      appBar: AppBar(backgroundColor: whiteColor,),
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -24,28 +26,93 @@ class Player extends StatelessWidget {
                 child: const Icon(Icons.music_note),
               ),
             ),
-
             Expanded(
                 child: Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
                   color: whiteColor,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(16)
-                  )
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Music Name",
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(16))),
+              child: Column(
+                children: [
+                  Text("Music Name",
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: bgDarkColor,
-                        fontSize: 24
+                          fontWeight: FontWeight.bold,
+                          color: bgDarkColor,
+                          fontSize: 24)),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text("Artist Name",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: bgDarkColor,
+                          fontSize: 20)),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "0:0",
+                        style: TextStyle(
+                          color: bgDarkColor,
+                        ),
+                      ),
+                      Expanded(
+                          child: Slider(
+                              thumbColor: sliderColor,
+                              inactiveColor: bgColor,
+                              activeColor: sliderColor,
+                              value: 0.0,
+                              onChanged: (newValue) {})),
+                      Text(
+                        "4:00",
+                        style: ourStyle(
+                          color: bgDarkColor,
+                        ),
                       )
-                    )
-                  ],
-                ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.skip_previous_rounded,
+                            size: 40,
+                            color: bgDarkColor,
+                          )),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundColor: bgDarkColor,
+                        child: Transform.scale(
+                            scale: 2.5,
+                            child: IconButton(
+                              onPressed: () {}, 
+                              icon: const Icon(
+                                Icons.play_arrow_rounded,
+                                color: whiteColor,
+                              ), 
+                              )
+                            ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.skip_next_rounded,
+                            size: 40,
+                            color: bgDarkColor,
+                          ))
+                    ],
+                  )
+                ],
+              ),
             )),
           ],
         ),
